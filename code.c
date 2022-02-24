0ea4035576736e23719e86fd6a11dc2a2ccdf0e3
lariskovski <larissaporto@live.com>
2022-02-24 17:28:26 +0000

    Add malloc success check and free
    Remove unecessary comments

--- code.c
+++ code.c
@@ -3,26 +3,24 @@
 #include <sys/wait.h>
 #include <stdlib.h> // malloc
 
-// gcc code.c -o code -lreadline
-// Source: https://youtu.be/DdpQQOgwyy4?t=642
 
 char** parseString(char str[]){
-    // Parse input string into space separated array
-    // Example: "ls -lah" -> ["ls", "-lah"]
+    /* Parse input string into space separated array
+    Example: "ls -lah" -> ["ls", "-lah"] */
 
     int i = 0;
     char** array = (char**) malloc(sizeof(char) * 50);
-
-    array[0] = strtok(str, " ");
-
-    // Loop is specific to strtok behaviour
-    while(array[i] != NULL){
-        array[++i] = strtok(NULL, " ");
+    // malloc returns NULL when failed to allocate memory
+    if (array != NULL){ 
+        array[0] = strtok(str, " ");
+        // Specific to strtok behaviour
+        while(array[i] != NULL){
+            array[++i] = strtok(NULL, " ");
+        }
     }
-
     return array;
-
 }
+
 
 int main(){
     int cpid;
@@ -39,6 +37,7 @@ int main(){
             execvp(parsedCommand[0], parsedCommand);  // ls -lah
             //execv(parsedCommand[0], parsedCommand); // /bin/ls -lah
         } else{
+            free(parsedCommand);
             wait((int *)NULL);
         }
     }
